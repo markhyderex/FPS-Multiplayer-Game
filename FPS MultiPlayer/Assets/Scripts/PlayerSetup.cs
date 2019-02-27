9b22ec42f740491e9372cb45da2a61eadd3de06d
JWooo <J_WooSP@hotmail.com>
2019-02-28 05:56:39 +0800

    Finished (?) shooting

--- FPS MultiPlayer/Assets/Scripts/PlayerSetup.cs
+++ FPS MultiPlayer/Assets/Scripts/PlayerSetup.cs
@@ -9,6 +9,9 @@ public class PlayerSetup : NetworkBehaviour
     [SerializeField] 
     Behaviour[] compToDisable;
 
+    [SerializeField]
+    string remoteLayerName = "RemotePlayer";
+
     public Camera sceneCamera;
 
     void Start()
@@ -16,21 +19,45 @@ public class PlayerSetup : NetworkBehaviour
         //check if we are controlling the player
         if (!isLocalPlayer)
         { //if not, disabled these componenets
-            for (int i = 0; i < compToDisable.Length; i++)
-            {
-                compToDisable[i].enabled = false;
-            }
+
+            // He moved to DisableComponents(){}
+            //for (int i = 0; i < compToDisable.Length; i++)
+            //{
+            //    compToDisable[i].enabled = false;
+            //}
+
+            DisableComponents();
+            AssignRemoteLayer();
         }
         else if (isLocalPlayer)
         {
-            print("sdsad");
             sceneCamera = Camera.main;
             if (sceneCamera != null)
             {
                 sceneCamera.gameObject.SetActive(false);
             }
-            Camera.main.gameObject.SetActive(true);
         }
+
+        RegisterPlayer();
+    }
+
+    void RegisterPlayer()
+    {
+        string _ID = "Player " + GetComponent<NetworkIdentity>().netId;
+        transform.name = _ID;
+    }
+
+    void DisableComponents()
+    {
+        for (int i = 0; i < compToDisable.Length; i++)
+        {
+            compToDisable[i].enabled = false;
+        }
+    }
+
+    void AssignRemoteLayer()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
     }
 
     private void OnDisable()
---
+++ FPS MultiPlayer/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    private const string PLAYER_TAG = "Player";
+
+    public PlayerWeapon weapon;
+
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private LayerMask mask;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        if (cam == null)
+        {
+            Debug.LogError("PlayerShoot: No camera referenced!");
+            this.enabled = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    [Client]
+    void Shoot()
+    {
+        RaycastHit _hit;
+        Debug.DrawRay(transform.position, Vector3.forward, Color.green);
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.range, mask))
+        {
+            if (_hit.collider.tag == PLAYER_TAG)
+            {
+                CmdPlayerShot(_hit.collider.name);
+            }
+        }
+    }
+
+    [Command]
+    void CmdPlayerShot(string _ID)
+    {
+        Debug.Log(_ID + " has been shot.");
+
+        //Destroy (GameObject.Find(_ID));
+    }
+}
